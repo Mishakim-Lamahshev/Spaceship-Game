2ee5840e42d537bcf735c604655040ca41bc6862
Ofek Rotem <92383710+ofekrotem@users.noreply.github.com>
2024-01-21 14:20:32 +0200

    lint

--- Assets/GoToGameOver.cs
+++ Assets/GoToGameOver.cs
@@ -1,35 +1,42 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GotoGameOverHpTrigger : MonoBehaviour {
+public class GotoGameOverHpTrigger : MonoBehaviour
+{
     [SerializeField] string sceneName;
 
 
     // Reference to the DestroyOnTrigger2D script attached to the same GameObject
     private HpManager hpManager;
 
-    private void Start(){
+    private void Start()
+    {
         // Try to get the HpManager component
         hpManager = GetComponent<HpManager>();
 
         // Check if the component is present
-        if (hpManager == null){
+        if (hpManager == null)
+        {
             Debug.LogError("HpManager component not found on the same GameObject.");
         }
     }
 
-    private void Update(){
-             // Check if DestroyOnTrigger2D component is present
-            if (hpManager != null){
-                // Check if the player is alive before triggering game over
-                if (hpManager.isDead){
-                    transform.position = Vector3.zero;
-                    SceneManager.LoadScene(sceneName);    // Input can either be a serial number or a name; here we use name.
-                }
+    private void Update()
+    {
+        // Check if DestroyOnTrigger2D component is present
+        if (hpManager != null)
+        {
+            // Check if the player is alive before triggering game over
+            if (hpManager.isDead)
+            {
+                transform.position = Vector3.zero;
+                SceneManager.LoadScene(sceneName);    // Input can either be a serial number or a name; here we use name.
             }
-            else{
-                Debug.LogError("HpManager component not found. Cannot check HP for game over.");
-            }
-            
         }
+        else
+        {
+            Debug.LogError("HpManager component not found. Cannot check HP for game over.");
+        }
+
     }
+}
